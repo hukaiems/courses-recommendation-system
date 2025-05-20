04b82ec05d29512840cf1ad91992f8a1dcbdc16c
hukaiems <nguoinhen0105@gmail.com>
2025-05-20 11:35:20 +0700

    add api overall dashboard

--- src/app/components/visualization-tab.tsx
+++ src/app/components/visualization-tab.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MissingData from "./visualization-graph/missing-data";
 import ConsistencyChart from "./visualization-graph/consistency";
+import axios from "axios";
+
+// define api json overall data
+
+const defaultOverallData = {
+  user_count: 0,
+  course_count: 0,
+  school_count: 0,
+  teacher_count: 0,
+  field_count: 0,
+};
+
+type OverallData = typeof defaultOverallData;
 
 // Main Visualization Component
 export default function VisualizationTab() {
+  const [overallData, setOverallData] =
+    useState<OverallData>(defaultOverallData);
   const [activeTab, setActiveTab] = useState("missing");
 
   // Render the active chart based on the selected tab
@@ -20,25 +35,57 @@ export default function VisualizationTab() {
     }
   };
 
+  // get overall data
+
+  useEffect(() => {
+    const fetchOverallData = async () => {
+      try {
+        const response = await axios.get(
+          "https://cs313-backend.onrender.com/api/overall"
+        );
+        // errors because API give array of so take the first one
+        setOverallData(response.data.data[0]);
+      } catch (err) {
+        console.log("Failed to get data: ", err);
+      }
+    };
+    fetchOverallData();
+  }, []);
+
   return (
     <div className="flex flex-col bg-blue-100 p-4 h-screen">
       {/* Stats summary */}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-6">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mb-6">
         <div className="bg-white p-4 rounded-md shadow">
-          <h3 className="text-gray-600 font-bold">Total Courses</h3>
-          <p className="text-2xl font-bold text-black">3000</p>
+          <h3 className="text-gray-600 font-bold">Users</h3>
+          <p className="text-2xl font-bold text-black">
+            {overallData.user_count}
+          </p>
         </div>
         <div className="bg-white p-4 rounded-md shadow">
-          <h3 className="text-gray-600 font-bold">Active Students</h3>
-          <p className="text-2xl font-bold text-black">23400</p>
+          <h3 className="text-gray-600 font-bold">Courses</h3>
+          <p className="text-2xl font-bold text-black">
+            {overallData.course_count}
+          </p>
         </div>
         <div className="bg-white p-4 rounded-md shadow">
-          <h3 className="text-gray-600 font-bold">Completion rates</h3>
-          <p className="text-2xl font-bold text-black">76%</p>
+          <h3 className="text-gray-600 font-bold">School</h3>
+          <p className="text-2xl font-bold text-black">
+            {overallData.school_count}
+          </p>
         </div>
         <div className="bg-white p-4 rounded-md shadow">
-          <h3 className="text-gray-600 font-bold">Average exercise</h3>
-          <p className="text-2xl font-bold text-black">56%</p>
+          <h3 className="text-gray-600 font-bold">Teacher</h3>
+          <p className="text-2xl font-bold text-black">
+            {overallData.teacher_count}
+          </p>
+        </div>
+
+        <div className="bg-white p-4 rounded-md shadow">
+          <h3 className="text-gray-600 font-bold">Field</h3>
+          <p className="text-2xl font-bold text-black">
+            {overallData.field_count}
+          </p>
         </div>
       </div>
 
